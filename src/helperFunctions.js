41ae4d7eac82ab319f69f3bca1b89e7fb4814a71
Ankit pal <palankit35@gmail.com>
2020-05-23 05:09:35 +0530

    Adding async/await

--- src/helperFunctions.js
+++ src/helperFunctions.js
@@ -10,7 +10,29 @@ const renderResponse = (res) => {
 
 // Manipulates responseField to render an unformatted response
 const renderRawResponse = (res) => {
-  // Displays either message depending on results
+  // Displays either message depending on// Manipulates responseField to render a formatted and appropriate message
+  const renderResponse = (res) => {
+    // Displays either message depending on results
+    if (res.errors) {
+      responseField.innerHTML = "<p>Sorry, couldn't format your URL.</p><p>Try again.</p>";
+    } else {
+      responseField.innerHTML = `<p>Your shortened url is: </p><p> ${res.shortUrl} </p>`;
+    }
+  }
+
+  // Manipulates responseField to render an unformatted response
+  const renderRawResponse = (res) => {
+    // Displays either message depending on results
+    if (res.errors) {
+      responseField.innerHTML = "<p>Sorry, couldn't format your URL.</p><p>Try again.</p>";
+    } else {
+      // Adds line breaks for JSON
+      let structuredRes = JSON.stringify(res).replace(/,/g, ", \n");
+      structuredRes = `<pre>${structuredRes}</pre>`;
+      responseField.innerHTML = `${structuredRes}`;
+    }
+  }
+ results
   if(res.errors){  
     responseField.innerHTML = "<p>Sorry, couldn't format your URL.</p><p>Try again.</p>";
   } else {
