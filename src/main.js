4ee50de2cc49da575a0d31cd8ed4f36f72f58e99
howdyAnkit <palankit35@gmail.com>
2020-10-11 02:42:02 +0530

    PWA finished

--- src/main.js
+++ src/main.js
@@ -43,3 +43,14 @@ const displayShortUrl = (event) => {
 }
 
 shortenButton.addEventListener('click', displayShortUrl);
+
+if("serviceWorker" in navigator){
+        navigator.serviceWorker.register('sw.js').then(registration => {
+            console.log("SW Registered!");
+            console.log(registration);
+        }).catch(error => {
+        console.log("SW Registration Failed");
+        console.log(error);
+    });
+}
+
